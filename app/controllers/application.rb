36cd42f63b5b242008cd0750057366d927ba02ad
olly <olly@3f70fff8-19eb-0310-b866-f8db04e603ad>
2007-08-12 14:28:16 +0000

    Show a login box, for basic authentication, when accessing atom or xml view through a browser.
    git-svn-id: http://svn.collaboa.org/svn/collaboa/branches/rewrite@664 3f70fff8-19eb-0310-b866-f8db04e603ad

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   def not_permitted
     respond_to do |format|
       format.html { redirect_to login_path}
-      format.xml {render(:text => "Access Denied\n", :status => 401)}
-      format.atom {render(:text => "Access Denied\n", :status => 401)}
+      format.xml {basic_authentication}
+      format.atom {basic_authentication}
     end
   end
   
@@ -19,5 +19,11 @@ class ApplicationController < ActionController::Base
   
   def ensure_user_has_permission_to(permission)
     not_permitted and return unless current_user.has_permission_to?(permission, :for => @project)
+  end
+  
+  def basic_authentication
+    headers["Status"]           = "Unauthorized"
+    headers["WWW-Authenticate"] = %(Basic realm="Collaboa Login")
+    render(:text => "Access Denied\n", :status => 401)
   end
 end
