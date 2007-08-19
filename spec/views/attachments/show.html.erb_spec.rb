787f11f3f500f63d9862ec4e71e7e37678861d80
olly <olly@3f70fff8-19eb-0310-b866-f8db04e603ad>
2007-08-19 20:39:32 +0000

    Added attachment view specification.
    git-svn-id: http://svn.collaboa.org/svn/collaboa/branches/rewrite@676 3f70fff8-19eb-0310-b866-f8db04e603ad

--- spec/views/attachments/show.html.erb_spec.rb
+++ spec/views/attachments/show.html.erb_spec.rb
@@ -4,14 +4,37 @@ describe "/attachments/show.html.erb" do
   include AttachmentsHelper
   
   before do
-    @attachment = mock_model(Attachment)
-
+    
+    @ticket = mock_model(Ticket)
+    @ticket_comment = mock_model(TicketComment, :ticket => @ticket, :author_text => 'Bob', :created_at => Time.gm(2007, 8, 19, 21, 05, 00))
+    @attachment = mock_model(Attachment, :filename => 'new_feature_patch.diff', :size => 1024, :ticket_comment => @ticket_comment, :public_filename => '../spec/fixtures/new_feature_patch.diff')
+        
+    assigns[:project] = mock_model(Project)
     assigns[:attachment] = @attachment
   end
 
-  it "should render attributes in <p>" do
+  it "should render filename property" do
     render "/attachments/show.html.erb"
-
+        
+    response.should have_tag('dl') do
+      with_tag('dt', 'Filename:')
+      with_tag('dd', 'new_feature_patch.diff')
+    end
+    
+    response.should have_tag('dl') do
+      with_tag('dt', 'Size:')
+      with_tag('dd', '1 KB')
+    end
+    
+    response.should have_tag('dl') do
+      with_tag('dt', 'Uploaded By:')
+      with_tag('dd', 'Bob')
+    end
+    
+    response.should have_tag('dl') do
+      with_tag('dt', 'Uploaded At:')
+      with_tag('dd', Time.gm(2007, 8, 19, 21, 05, 00).to_s)
+    end
   end
 end
 
